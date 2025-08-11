27b88432f1de210f53703245f21c79bd2ac1f83f
Akshit Panchal <akshitpp01@gmail.com>
2025-08-11 15:02:48 +0530

    Some Issue Resolve

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/', schoolRouter)
 
 const PORT = process.env.PORT || 4041
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
     console.log(`Server is running on ${PORT}`)
 })
 
